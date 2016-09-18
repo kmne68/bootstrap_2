c4588ae3212e7623e97e376b14ffab7fea215f9c
kmne68 <kmne68@gmail.com>
2016-09-18 14:16:23 -0500

    Add project files.

---
+++ Bootstrap_Practice_2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bootstrap_Practice_2.Startup))]
+namespace Bootstrap_Practice_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
